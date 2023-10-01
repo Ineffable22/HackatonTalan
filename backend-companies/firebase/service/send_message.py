a0f8a54a95dcfdd10428264f8d731ec83f1d3db9
Carlos Soria <soriac367@gmail.com>
2023-10-01 03:40:45 -0500

    Correcion

--- backend-companies/firebase/service/send_message.py
+++ backend-companies/firebase/service/send_message.py
@@ -1,12 +1,17 @@
 """Service to send push notification"""
 from firebase_admin import messaging
 from firebase.config import firebase_app
+import json
 
 def send_topic_push(title: str, body: dict, fcm_token: str):
+
+    print(title)
+    print(body)
+    print(fcm_token)
     message = messaging.Message(
         notification=messaging.Notification(
             title=title,
-            body=body
+            body=json.dumps(body)
         ),
         token=fcm_token
     )
