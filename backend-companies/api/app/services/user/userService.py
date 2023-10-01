6551e62c97435e64f5e8346e6a5c0a6b48e7b84b
Humberto Tejada Espinoza <90359858+humtej1204@users.noreply.github.com>
2023-10-01 01:19:41 -0500

    Update userService.py

--- backend-companies/api/app/services/user/userService.py
+++ backend-companies/api/app/services/user/userService.py
@@ -1,6 +1,5 @@
 from fastapi import HTTPException
 from api.app.models.dto.userModels import UserLoginDto
-from api.app.models.entity.user import User
 from api.app.repositories.user import UserRepository
 
 userRepo = UserRepository()
@@ -10,11 +9,11 @@ class UserService:
         user = userRepo.get_by_email(user_login.email)
         if (not user):
             error_message = "El correo Ingresado no es valido"
-            raise HTTPException(status_code=500, detail={"error_code": "050", "error_message": error_message})
+            raise HTTPException(status_code=400, detail={"code": "050", "message": error_message})
         
         if (user_login.password != user["password"]):
             error_message = "La contraseña ingresada no es correcta"
-            raise HTTPException(status_code=500, detail={"error_code": "050", "error_message": error_message})
+            raise HTTPException(status_code=400, detail={"code": "050", "message": error_message})
         
         if ((not user["fcmtoken"]) or (user["fcmtoken"] == "") or (user["fcmtoken"] != user_login.fcmtoken)):
             user = userRepo.update(user["id"], {"fcmtoken": user_login.fcmtoken})
